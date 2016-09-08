cbc3fd00eeae687751ffb5d4b4b417d47bfe0367
Karim Khenchoucha <khenchouchakarim@gmail.com>
2016-09-08 14:17:49 +0200

    changement des meta

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@
 ;
 // Lightbox gallery (karim)
 
-// rediriger  le clik allant vers le lien , vers une image
-   //    montrer le Lightbox
-   //    charger dans le Lightbox l'image du lien
-   //    obtenir l'attribut alt des enfants et mettre un caption
+// rediriger le clik allant vers le lien , vers une image
+   $("#galleryphoto a").click(function(event);{
+      event.preventDefault();
+      var href = $(this).attr("href");
+         console.log(href);
+      //    montrer le Lightbox
+      //    charger dans le Lightbox l'image du lien
+      //    obtenir l'attribut alt des enfants et mettre un caption
+
+   });
 
 // montrer la Lightbox
    // ajouter l'image à la Lightbox
